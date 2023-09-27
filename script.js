6fbbb6215a84f64932a5c973d104ff6b44e327d4
Talha <talha18513@gmail.com>
2023-09-27 03:01:14 -0700

    first commit

--- script.js
+++ script.js
@@ -18,4 +18,30 @@ function toggleNavMid() {
   
   window.addEventListener('load', toggleNavMid);
   window.addEventListener('resize', toggleNavMid);
-  
+
+
+  // =================nav end===============
+ 
+  function adjustBrTags() {
+    var screenWidth = window.innerWidth;
+
+    // Get the h1 and p elements
+    var h1Element = document.querySelector('.info h1');
+    var pElement = document.querySelector('.info p');
+
+    if (screenWidth < 700 && screenWidth > 428) {
+        // Add <br> tags to both h1 and p elements
+        h1Element.innerHTML = "Let’s Create our <br> Story Together!";
+        pElement.innerHTML = "Vr story is an interactive tool that makes it easy for <br> anyone to produce engaging digital experiences";
+    } else {
+        // Remove <br> tags from both h1 and p elements
+        h1Element.innerHTML = "Let’s Create our Story Together!";
+        pElement.innerHTML = "Vr story is an interactive tool that makes it easy for anyone to produce engaging digital experiences";
+    }
+}
+
+// Initial call to adjustBrTags on page load
+adjustBrTags();
+
+// Add an event listener to adjustBrTags when the window is resized
+window.addEventListener('resize', adjustBrTags);
